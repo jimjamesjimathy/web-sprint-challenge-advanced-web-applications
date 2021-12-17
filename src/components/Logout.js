01ef2d0e6eebe1eae8110b9e7d915aca20c65844
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2021-12-17 03:00:26 -0500

    All functionality is working. Will move on to testing tomorrow

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
+
 
 const Logout = () => {        
+    const { push } = useHistory();
+    const token = localStorage.getItem('token')
+    useEffect(() => {
+        axiosWithAuth().post('/logout', {}, {
+            headers: {
+                authorization: token
+            }
+        })
+        .then(res => {
+            localStorage.removeItem('token');
+            push('/login')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    })
+
     return(<div></div>);
 }
 
 export default Logout;
-
-// Task List
-// 1. On mount, execute a http request to the logout endpoint.
-// 2. On a successful request, remove the token from localStorage and redirect to the login page.
