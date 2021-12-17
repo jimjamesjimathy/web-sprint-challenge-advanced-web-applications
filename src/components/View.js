60cfb64f6e5f84ab30064912d486098aeccedb88
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2021-12-16 23:19:55 -0500

    Delete is working

--- src/components/View.js
+++ src/components/View.js
@@ -5,12 +5,47 @@ import axiosWithAuth from '../utils/axiosWithAuth';
 import Article from './Article';
 import EditForm from './EditForm';
 
+
+
+const Container = styled.div`
+    padding: 0.5em;
+`
+const HeaderContainer = styled.h1`
+    border-bottom: solid black 2px;
+    padding: 1em;
+    margin:0;
+    font-size: 1.5em;
+    background: black;
+    color: white;
+`
+
+const ArticleDivider = styled.div`
+    border-bottom: 1px solid black;
+    padding: 1em;
+`
+
+const ComponentContainer = styled.div`
+    display:flex;
+    width: 80%;
+    flex-direction: column;
+    justify-content: center;
+    
+`
+
+const ContentContainer = styled.div`
+    display: flex;
+    flex-direction: ${props => props.flexDirection};
+`
+
+const ArticleContainer = styled.div`
+    background: grey;
+`;
+
 const View = (props) => {
     const [articles, setArticles] = useState([]);
     const [editing, setEditing] = useState(false);
     const [editId, setEditId] = useState();
     const token = localStorage.getItem('token');
-
 
     useEffect(() => {
         axiosWithAuth().get('/articles', {
@@ -24,13 +59,8 @@ const View = (props) => {
             .catch(err => console.log(err.response.data))
     }, []);
 
-
-
-
-
-
-
     const handleDelete = (id) => {
+        setArticles(articles.filter(article => (article.id !== id)));
     }
 
     const handleEdit = (article) => {
@@ -67,47 +97,5 @@ const View = (props) => {
 
 export default View;
 
-//Task List:
-//1. Build and import axiosWithAuth module in the utils.  xxx
-
-
-
-
-//2. When the component mounts, make an http request that adds all articles to state.
 //3. Complete handleDelete method. It should make a request that delete the article with the included id.
 //4. Complete handleEdit method. It should make a request that updates the article that matches the included article param.
-
-
-const Container = styled.div`
-    padding: 0.5em;
-`
-const HeaderContainer = styled.h1`
-    border-bottom: solid black 2px;
-    padding: 1em;
-    margin:0;
-    font-size: 1.5em;
-    background: black;
-    color: white;
-`
-
-const ArticleDivider = styled.div`
-    border-bottom: 1px solid black;
-    padding: 1em;
-`
-
-const ComponentContainer = styled.div`
-    display:flex;
-    width: 80%;
-    flex-direction: column;
-    justify-content: center;
-    
-`
-
-const ContentContainer = styled.div`
-    display: flex;
-    flex-direction: ${props => props.flexDirection};
-`
-
-const ArticleContainer = styled.div`
-    background: grey;
-`;
