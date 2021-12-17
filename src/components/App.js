5904e81ca532d1181338512927dd38d3c32af35f
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2021-12-16 21:02:27 -0500

    All Routes built. Moving right along

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
-
+import PrivateRoute from './PrivateRoute';
 import Header from './Header';
 import BloomHeader from './BloomHeader';
 import Login from './Login';
+import View from './View';
+import Logout from './Logout';
+
+
+
+
 
 const App = () => {
   return (
@@ -12,9 +18,14 @@ const App = () => {
       <BloomHeader/>
       <Header/>
       <RouteContainer>
-        <Route exact path="/">
+        <Route exact path='/'>
           <Login/>
-        </Route>          
+        </Route> 
+        <Route exact path='/login'>
+          <Login/>
+        </Route>
+        <PrivateRoute exact path='/view' component={View} />
+        <PrivateRoute exact path='/logout' component={Logout} />                
       </RouteContainer>
     </AppContainer>
   )
@@ -23,10 +34,10 @@ const App = () => {
 export default App;
 
 //Task List
-//1. Create and import PrivateRoute component.
-//2. Create a Route for Login pointing to '/login.'
-//3. Create a PrivateRoute for View component point to '/view.'
-//4. Create a PrivateRoute for Logout component pointing to '/logout.'
+//1. Create and import PrivateRoute component. x
+//2. Create a Route for Login pointing to '/login.' x
+//3. Create a PrivateRoute for View component point to '/view.' x
+//4. Create a PrivateRoute for Logout component pointing to '/logout.' x
 
 
 const AppContainer = styled.div`
